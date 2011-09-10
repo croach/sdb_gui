1a75918ddbb2e7ef09c7d672ad1ba5601eef8689
croach <vthakr@gmail.com>
2011-09-10 04:43:36 -0700

    Updated project.clj with macwidgets and forms libraries from the Maven repository and Amazon's AWS library

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject sdb_gui "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [com.jgoodies/forms "1.2.1"]
+                 [com.kenai.nbpwr/com-explodingpixels-macwidgets
+                  "0.9.5-201002261215"]
+                 [org.clojars.tltjr/aws-java-sdk "1.1.7.1"]]
   :main sdb_gui.core)
