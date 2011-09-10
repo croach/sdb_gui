a61abb54a803b84d40ada4b2e22e588ffe63af6c
Christopher Roach <vthakr@gmail.com>
2011-09-09 20:08:27 -0700

    Added the macwidgets and forms libraries to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject sdb_gui "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojars.nakkaya/forms "1.2.1"]
+                 [org.clojars.nakkaya/macwidgets "0.9.5"]]
   :main sdb_gui.core)
