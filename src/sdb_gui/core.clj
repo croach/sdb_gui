b8207451f028022812106974c8cf9120b13194e8
croach <vthakr@gmail.com>
2011-09-19 00:15:01 -0700

    A little refactoring of the main function

--- src/sdb_gui/core.clj
+++ src/sdb_gui/core.clj
@@ -9,23 +9,7 @@
                                            SourceListCategory
                                            SourceListControlBar
                                            MacWidgetFactory
-                                           MacIcons))
-    (:gen-class))
-
-(def mainframe (frame
-                :title    "SDB Viewer"
-                :size     [700 :by 400]
-                :on-close :exit))
-
-
-(defn create-source-list []
-  (let [category (SourceListCategory. "Accounts")
-        model    (doto (SourceListModel.)
-                   (.addCategory category))
-        accounts (accounts/get-account-names)]
-    (doseq [account-name accounts]
-      (.addItemToCategory model (SourceListItem. account-name) category))
-    (SourceList. model)))
+                                           MacIcons)))
 
 (defn edit-account-panel []
   (mig-panel
@@ -43,39 +27,65 @@
                      (button :text "Connect")]) "span"]]))
 
 (defn update-account
-     "Add a new AWS account"
-  []
+  "Add a new AWS account"
+  [mainframe]
   (let [content-panel (select mainframe [:#content-panel])]
     (show-card! content-panel :account)))
 
 
-(defn create-control-bar []
+(defn create-source-list
+  [control-bar]
+  (let [category (SourceListCategory. "Accounts")
+        model    (doto (SourceListModel.)
+                   (.addCategory category))
+        accounts (accounts/get-account-names)]
+    (doseq [account-name accounts]
+      (.addItemToCategory model (SourceListItem. account-name) category))
+    (doto (SourceList. model)
+      (.installSourceListControlBar control-bar))))
+
+(defn create-control-bar
+  [mainframe]
   (doto (SourceListControlBar.)
     (.createAndAddButton
      MacIcons/PLUS
-     (action :handler (fn [e] (update-account))))
+     (action :handler (fn [e] (update-account mainframe))))
     (.createAndAddButton
      MacIcons/MINUS
      (action :handler (fn [e] (println "Removing an account"))))))
 
-(defn create-gui [mainframe]
-  (let [control-bar (create-control-bar)
-        source-list (create-source-list)
-        split-pane  (. MacWidgetFactory createSplitPaneForSourceList
-                       source-list
-                       (card-panel
-                        :id :content-panel
-                        :items
-                        [[(flow-panel) :blank]
-                         [(edit-account-panel) :account]]))]
+(defn create-content-panel []
+  (card-panel
+   :id :content-panel
+   :items
+   [[(flow-panel) :blank]
+    [(edit-account-panel) :account]]))
+
+(defn create-split-pane
+  [source-list content-panel]
+  (doto (. MacWidgetFactory createSplitPaneForSourceList
+           source-list
+           content-panel)
+    (.setDividerLocation 250)))
+
+
+(defn create-main-frame []
+  (frame
+   :title    "SDB Viewer"
+   :size     [700 :by 400]
+   :on-close :exit))
+
+(defn create-gui []
+  (let [mainframe     (create-main-frame)
+        control-bar   (create-control-bar mainframe)
+        source-list   (create-source-list control-bar)
+        content-panel (create-content-panel)
+        split-pane    (create-split-pane source-list content-panel)]
     (.installDraggableWidgetOnSplitPane control-bar split-pane)
-    (.installSourceListControlBar source-list control-bar)
-    (.setDividerLocation split-pane 250)
     (.setContentPane mainframe split-pane)
     mainframe))
 
 
 (defn -main [& args]
-  (invoke-later
-   (show!
-    (create-gui mainframe))))
+  (show!
+   (create-gui)))
